4cb7d1e8850028a387dc820fc2276a9f381564bb
Damien Vesper <ldamienvesper@gmail.com>
2025-01-20 11:32:30 -0600

    core bun attempt

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,9 @@
 import { Webview } from "webview-bun";
+import { file, resolve } from "bun";
 
-import "./core";
+import { core } from "./core";
 
 export const view = new Webview();
+
+view.setHTML(await file(await resolve(`../src/index.html`, process.cwd())).text());
+view.run();
